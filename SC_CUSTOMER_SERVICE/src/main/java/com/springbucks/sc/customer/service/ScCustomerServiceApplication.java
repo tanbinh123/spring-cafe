c9013f0daeebf5d9021de09004f176075851ce3c
Boyang Li <bryanli2009@live.ca>
2019-08-02 16:05:52 +0800

    Add waiter and customer service sub-modules

---
+++ SC_CUSTOMER_SERVICE/src/main/java/com/springbucks/sc/customer/service/ScCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springbucks.sc.customer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScCustomerServiceApplication.class, args);
+	}
+
+}
